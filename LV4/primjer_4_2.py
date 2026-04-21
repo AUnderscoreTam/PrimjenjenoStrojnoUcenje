570b9ac93ae49b16b4e9aab6dbc30b8fc5d3bde6
AUnderscoreTam <denykeckes@gmail.com>
2026-04-21 23:14:02 +0200

    lv4 zad3 dovrsen

--- LV4/primjer_4_2.py
+++ LV4/primjer_4_2.py
@@ -4,56 +4,67 @@ import sklearn.linear_model as lm
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import PolynomialFeatures
 
+
 def non_func(x):
-    y = 1.6345 - 0.6235*np.cos(0.6067*x) - 1.3501*np.sin(0.6067*x) - 1.1622 * np.cos(2*x*0.6067) - 0.9443*np.sin(2*x*0.6067)
+    y = 1.6345 - 0.6235*np.cos(0.6067*x) - 1.3501*np.sin(0.6067*x) - \
+        1.1622 * np.cos(2*x*0.6067) - 0.9443*np.sin(2*x*0.6067)
     return y
+
 
 def add_noise(y):
     np.random.seed(14)
     varNoise = np.max(y) - np.min(y)
-    y_noisy = y + 0.1*varNoise*np.random.normal(0,1,len(y))
+    y_noisy = y + 0.1*varNoise*np.random.normal(0, 1, len(y))
     return y_noisy
- 
-x = np.linspace(1,10,50)
-y_true = non_func(x)
-y_measured = add_noise(y_true)
 
-x = x[:, np.newaxis]
-y_measured = y_measured[:, np.newaxis]
 
-# make polynomial features
-poly = PolynomialFeatures(degree=15)
-xnew = poly.fit_transform(x)
-    
-np.random.seed(12)
-indeksi = np.random.permutation(len(xnew))
-indeksi_train = indeksi[0:int(np.floor(0.7*len(xnew)))]
-indeksi_test = indeksi[int(np.floor(0.7*len(xnew)))+1:len(xnew)]
+features = [2, 6, 15]
+MSEtrain = []
+MSEtest = []
+lmArray = []
+xnewArray = []
 
-xtrain = xnew[indeksi_train,]
-ytrain = y_measured[indeksi_train]
+for number in features:
+    x = np.linspace(1, 10, 50)
+    y_true = non_func(x)
+    y_measured = add_noise(y_true)
 
-xtest = xnew[indeksi_test,]
-ytest = y_measured[indeksi_test]
+    x = x[:, np.newaxis]
+    y_measured = y_measured[:, np.newaxis]
 
-linearModel = lm.LinearRegression()
-linearModel.fit(xtrain,ytrain)
+    # make polynomial features
+    poly = PolynomialFeatures(degree=number)
+    xnew = poly.fit_transform(x)
+    xnewArray.append(xnew)
 
-ytest_p = linearModel.predict(xtest)
-MSE_test = mean_squared_error(ytest, ytest_p)
+    np.random.seed(12)
+    indeksi = np.random.permutation(len(xnew))
+    indeksi_train = indeksi[0:int(np.floor(0.7*len(xnew)))]
+    indeksi_test = indeksi[int(np.floor(0.7*len(xnew)))+1:len(xnew)]
 
-plt.figure(1)
-plt.plot(xtest[:,1],ytest_p,'og',label='predicted')
-plt.plot(xtest[:,1],ytest,'or',label='test')
-plt.legend(loc = 4)
-plt.show()
+    xtrain = xnew[indeksi_train,]
+    ytrain = y_measured[indeksi_train]
 
-#pozadinska funkcija vs model
-plt.figure(2)
-plt.plot(x,y_true,label='f')
-plt.plot(x, linearModel.predict(xnew),'r-',label='model')
+    xtest = xnew[indeksi_test,]
+    ytest = y_measured[indeksi_test]
+
+    linearModel = lm.LinearRegression()
+    linearModel.fit(xtrain, ytrain)
+    lmArray.append(linearModel)
+    ytest_p = linearModel.predict(xtest)
+    ytrain_p = linearModel.predict(xtrain)
+    MSE_test = mean_squared_error(ytest, ytest_p)
+    MSEtest.append(MSE_test)
+    MSE_train = mean_squared_error(ytrain, ytrain_p)
+    MSEtrain.append(MSE_train)
+
+    # pozadinska funkcija vs model
+plt.plot(x, y_true, label='f')
+for index in range(3):
+    plt.plot(x, lmArray[index].predict(
+        xnewArray[index]), label='features ' + str(features[index]))
 plt.xlabel('x')
 plt.ylabel('y')
-plt.plot(xtrain[:,1],ytrain,'ok',label='train')
-plt.legend(loc = 4)
+plt.plot(xtrain[:, 1], ytrain, 'ok', label='train')
+plt.legend(loc=4)
 plt.show()
--- LV4/primjer_4_3.py
+++ LV4/primjer_4_3.py
@@ -3,24 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#1 6699 automobila
+# 1 6699 automobila
 """
-2 str    
-int64  
+2 str
+int64
 float64
-int64  
-str    
-str    
-str    
-str    
+int64
+str
+str
+str
+str
 float64
-int64  
+int64
 float64
-int64 
+int64
 """
-#3 automobil 2591 najvecu, a automobil 4778 najmanju cijenu
-#4 575
-#5 automobil 3067 najvise, a autmobil 6514 najmanje km
+# 3 automobil 2591 najvecu, a automobil 4778 najmanju cijenu
+# 4 575
+# 5 automobil 3067 najvise, a autmobil 6514 najmanje km
 
 # ucitavanje ociscenih podataka
 df = pd.read_csv('cars_processed.csv')
@@ -28,15 +28,14 @@ print(df.info())
 
 print(df.selling_price.idxmax())
 print(df.selling_price.idxmin())
-count=0
+count = 0
 for year in df.year:
     if year == 2012:
-        count+=1
+        count += 1
 print(count)
 
 print(df.km_driven.idxmax())
 print(df.km_driven.idxmin())
 
-dfseats=df.groupby(['seats']).agg(total_seats=('seats','sum'))
+dfseats = df.groupby(['seats']).agg(total_seats=('seats', 'sum'))
 print(dfseats.info)
-
